5014d3c1d6bb683e4b4ed9252927270016059373
sotowang <sotowang@qq.com>
2018-10-02 21:54:43 +0800

    getTable

--- src/test/java/com/soto/HBaseConnTest.java
+++ src/test/java/com/soto/HBaseConnTest.java
@@ -1,7 +1,10 @@
 package com.soto;
 
 import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.Table;
 import org.junit.Test;
+
+import java.io.IOException;
 
 public class HBaseConnTest {
 
@@ -11,6 +14,15 @@ public class HBaseConnTest {
         System.out.println(conn.isClosed());
         HBaseConn.closeConn();
         System.out.println(conn.isClosed());
+    }
 
+    @Test
+    public void getTableTest() {
+        try {
+            Table table = HBaseConn.getTable("US_POPULATION");
+            System.out.println(table.getName().getNameAsString());
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
     }
 }
